b63d1c59259a91611165e3d73b317cd0498baa99
Bruno Muratore <bruno.muratore@outlook.com>
2019-02-22 19:17:29 +0100

    Store on caches until they are full
    Jump from top 2k to top 1.3k

--- HashCode2017Qualify/src/main/Solver.scala
+++ HashCode2017Qualify/src/main/Solver.scala
@@ -11,13 +11,20 @@ import util.control.Breaks._
 
 class Solver(caches: ArrayBuffer[Cache], endpoints: ArrayBuffer[Endpoint]) {
 
+
   def solve(): List[Cache] = {
 
     endpoints.filter(_.links.nonEmpty).foreach { endpoint =>
-      val closestCache = endpoint.links.minBy(_.latency).cache
-      endpoint.requests.sortBy(-_.totalSize).map(_.video).foreach(closestCache.addIfHasSpace)
+      val closestCaches = endpoint.links.sortBy(_.latency).map(_.cache)
+
+      endpoint.requests.sortBy(-_.totalSize).map(_.video).foreach{ video =>
+        val it = closestCaches.iterator
+        while(it.hasNext && !it.next().addIfHasSpace(video)){}
+      }
     }
     caches.toList
   }
+
+
 
 }
