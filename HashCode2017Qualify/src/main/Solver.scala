53838de3d5c21037de0e9ab2135806ccd51fd97d
Marcelo Ramires <marcra@microsoft.com>
2019-02-24 13:28:55 -0800

    Created unCachedVideos

--- HashCode2017Qualify/src/main/Solver.scala
+++ HashCode2017Qualify/src/main/Solver.scala
@@ -17,19 +17,36 @@ class Solver(caches: ArrayBuffer[Cache], endpoints: ArrayBuffer[Endpoint], in: I
     caches
   }
 
-  def distributeVideosToCaches() = {
-    endpoints.foreach { endpoint =>
+  def unCachedVideosForEndpoint(endpoint: Endpoint) = {
+    val allVideos = endpoint.requests.map(_.video).toSet
+    val cachedVideos = endpoint.caches.keys.flatMap(caches(_).cachedVideos.values).map(_.video).toSet
 
+    allVideos.diff(cachedVideos)
+  }
+
+  def distributeVideosToCaches() = {
+
+    val pb = new ProgressBar("Distributing", endpoints.size)
+
+    endpoints.foreach { endpoint =>
+      val unCachedVideos = unCachedVideosForEndpoint(endpoint)
       val bestCaches = endpoint.caches.toList.sortBy(_._2).map(kv => caches(kv._1))
       if (bestCaches.nonEmpty) {
-        endpoint.requests.foreach { req =>
+        endpoint.requests.filter(req => unCachedVideos.contains(req.video)).foreach { req =>
           val savedLatency = (endpoint.latency - bestCaches.head.getLatencyForEndpointId(endpoint.id)) * req.totalSize
             bestCaches.head.addToInfinityCache(req.video, savedLatency)
         }
       }
+      pb.update()
     }
 
     caches.foreach(_.trimCache())
   }
-
 }
+/*
+total: 1601000 ▲ 149000
+videos_worth_spreading.in: 422000 ▲ 253000
+me_at_the_zoo.in: 318000 ▼ -4000
+kittens.in: 524000 ▼ -80000
+trending_today.in: 337000 ▼ -134000
+*/
