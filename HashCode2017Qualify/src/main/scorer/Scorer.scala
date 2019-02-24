38c1e3b78ff0e7a57eb13a9a7dc4c3c7641585a7
Maicon Costa <maiconscosta@gmail.com>
2019-02-24 20:51:22 +0100

    Solve filter-map

--- HashCode2017Qualify/src/main/scorer/Scorer.scala
+++ HashCode2017Qualify/src/main/scorer/Scorer.scala
@@ -8,16 +8,23 @@ import scala.collection.mutable.ArrayBuffer
 object Scorer {
   def compute(endpoints: ArrayBuffer[Endpoint], caches: ArrayBuffer[Cache]): Score = {
     val max = 0
+    /*
+    var totalSaved = 0
+    var totalRequests = 0
 
-    /*val pb = new ProgressBar("Score", endpoints.size)
+    val pb = new ProgressBar("Score", endpoints.size)
     endpoints.foreach{ end =>
       end.requests.foreach{ req =>
-        totalSaved +=
-          //req.qty * (end.latency - end.caches.keys.filter(cacheId => caches(cacheId).videos.contains(req.video.id)).map(_.latency).sorted.headOption.getOrElse(end.latency))
+        totalSaved += req.qty *
+          (end.latency -
+                end.caches.keys.filter(
+                  cacheId =>  caches(cacheId).cachedVideos.contains(req.video.id)
+                ).map(_).sorted.headOption.getOrElse(end.latency))
         totalRequests += req.qty
       }
       pb.update()
-    }*/
+    }
+    */
 
     val totalSaved = caches.map(_.cachedVideos.values.map(_.savedLatency).sum).sum
     val totalRequests = endpoints.map(_.requests.length).sum
