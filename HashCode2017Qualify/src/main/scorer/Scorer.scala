8a3859441734983adc3cfd390784a9699c1e57e1
Bruno Muratore <bruno.muratore@outlook.com>
2019-02-24 20:56:14 +0100

    scorer

--- HashCode2017Qualify/src/main/scorer/Scorer.scala
+++ HashCode2017Qualify/src/main/scorer/Scorer.scala
@@ -8,7 +8,7 @@ import scala.collection.mutable.ArrayBuffer
 object Scorer {
   def compute(endpoints: ArrayBuffer[Endpoint], caches: ArrayBuffer[Cache]): Score = {
     val max = 0
-    /*
+
     var totalSaved = 0
     var totalRequests = 0
 
@@ -19,15 +19,14 @@ object Scorer {
           (end.latency -
                 end.caches.keys.filter(
                   cacheId =>  caches(cacheId).cachedVideos.contains(req.video.id)
-                ).map(_).sorted.headOption.getOrElse(end.latency))
+                ).map(cacheId => caches(cacheId).getLatencyForEndpointId(end.id)).toList.sorted.headOption.getOrElse(end.latency))
         totalRequests += req.qty
       }
       pb.update()
     }
-    */
 
-    val totalSaved = caches.map(_.cachedVideos.values.map(_.savedLatency).sum).sum
-    val totalRequests = endpoints.map(_.requests.length).sum
+//    val totalSaved = caches.map(_.cachedVideos.values.map(_.savedLatency).sum).sum
+//    val totalRequests = endpoints.map(_.requests.length).sum
 
     Score(totalSaved / totalRequests * 1000, max)
   }
