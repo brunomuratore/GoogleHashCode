9e4aa27396a517c50e8dc4e3c66c9f80329faa0a
Bruno Muratore <bruno.muratore@outlook.com>
2019-02-24 18:28:59 +0100

    initial solve

--- HashCode2017Qualify/src/main/Solver.scala
+++ HashCode2017Qualify/src/main/Solver.scala
@@ -16,25 +16,7 @@ class Solver(caches: ArrayBuffer[Cache], endpoints: ArrayBuffer[Endpoint], in: I
 
   def solve(): List[Cache] = {
 
-    val pb = new ProgressBar("Solver", endpoints.length)
-
-    val videoSet = Set[Int]()
-
-    endpoints.filter(_.links.nonEmpty).foreach { endpoint =>
-      val closestCaches = endpoint.links.sortBy(_.latency).map(_.cache)
-
-      endpoint.requests.sortBy(-_.totalSize).map(_.video).foreach{ video =>
-        if(List(1,3).contains(in) || !videoSet.contains(video.id)) {
-          val it = closestCaches.iterator
-          while (it.hasNext && !it.next().addIfHasSpace(video)) {}
-          videoSet += video.id
-        }
-      }
-
-      pb.update()
-    }
-
-    caches.toList
+    List[Cache]()
   }
 
 }
