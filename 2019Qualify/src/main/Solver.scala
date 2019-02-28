2f4f0b15e5db2d6bcee61ac8f871886065f0fca4
brunomuratore <bruno.muratore@schibsted.com>
2019-02-28 19:59:18 +0100

    PickSlidefortags

--- 2019Qualify/src/main/Solver.scala
+++ 2019Qualify/src/main/Solver.scala
@@ -10,13 +10,40 @@ import main.models.{Photo, Slide, SlideShow}
 import scala.collection.{mutable, _}
 import scala.util.Random
 
-class Solver(photos: Set[Photo], tagInPhotos: Map[String, Set[Photo]], sortedPhotos: mutable.TreeMap[Int, Array[Photo]])(implicit file: String) {
+class Solver(photos: Set[Photo], tagInPhotos: Map[String, Array[Photo]], sortedPhotos: mutable.TreeMap[Int, Array[Photo]])
+            (implicit file: String) {
+
   val r = new Random()
 
   def solve() = {
 
     run()
     SlideShow(mutable.ListBuffer.empty[Slide])
+  }
+
+  def pickVerticalPhoto(tags: Int): Option[Photo] = {
+    tags.to(0).by(-1).foreach { t =>
+      val diff = tags - t
+      for (photo <- sortedPhotos(tags))
+        if (photo.vertical) return Some(photo)
+    }
+    return None
+  }
+
+  def pickSlide(tags: Int): Option[Slide] = {
+    tags.to(0).by(-1).foreach { t =>
+      val diff = tags - t
+        for (photo <- sortedPhotos(tags))
+          if (!photo.vertical) return Some(Slide(List(photo)))
+          else if (diff > 0) {
+            val p = pickVerticalPhoto(tags(diff))
+            if(p.isDefined)
+              return Some(Slide(List(photo,p.get)))
+            else
+              return None
+          }
+
+    }
   }
 
   private def run() = {
--- 2019Qualify/src/main/models/Photo.scala
+++ 2019Qualify/src/main/models/Photo.scala
@@ -4,6 +4,6 @@ import scala.collection.mutable
 
 case class Photo(id: Int, vertical: Boolean, tags: Set[String])
 
-case class Slide(photos: mutable.ListBuffer[Photo])
+case class Slide(photos: List[Photo])
 
 case class SlideShow(slides: mutable.ListBuffer[Slide])
