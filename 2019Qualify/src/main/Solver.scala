587d5b62cb6f0d892196e09d4bc33ecee138d9ce
brunomuratore <bruno.muratore@schibsted.com>
2019-02-28 20:16:13 +0100

    asd

--- 2019Qualify/src/main/Solver.scala
+++ 2019Qualify/src/main/Solver.scala
@@ -33,7 +33,7 @@ class Solver(photos: Set[Photo], tagInPhotos: Map[String, Array[Photo]], sortedP
     tags.to(0).by(-1).foreach { t =>
       val diff = tags - t
       val photoIds = sortedPhotos(tags).indices
-      while(photoIds.nonEmpty) {
+      while (photoIds.nonEmpty) {
         val photo = sortedPhotos(tags)(photoIds(r.nextInt(photoIds.length)))
         if (!photo.vertical) return Some(Slide(List(photo)))
         else if (diff > 0) {
@@ -44,42 +44,14 @@ class Solver(photos: Set[Photo], tagInPhotos: Map[String, Array[Photo]], sortedP
             return None
         }
       }
-
+    }
     None
   }
 
-  private def run() = {
+  def run() = {
     val bar = new ProgressBar("Greedy solve", 1)
 
     bar.update()
-  }
-
-  def pickSlide(): Slide = {
-    val slide = new Slide(mutable.ListBuffer[Photo]())
-
-    // grab the max number of tags in a single photo
-    val max = sortedPhotos.keys.last
-
-    // grab the first photo which has this number of tags
-    val photo1 = sortedPhotos(max)(0)
-
-    // if it's horizontal return it
-    if (!photo1.vertical) {
-        slide.photos += photo1
-        return slide
-    }
-
-    // It's vertical, try to add the next one
-    for (i <- 1 until sortedPhotos(max).length) {
-      var photo2 = sortedPhotos(max)(i)
-      if (photo2.vertical) {
-        slide.photos += photo2
-        return slide
-      }
-    }
-
-    // it was not possible to find another vertical photo, just return a slide containing single photo
-    return slide
   }
 
 
