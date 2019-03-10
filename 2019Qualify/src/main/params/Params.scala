dc2add3b1ec6368a12232c414465bce1f702c61a
Bruno Muratore <bruno.muratore@outlook.com>
2019-03-10 16:48:27 +0100

    last changes

--- 2019Qualify/src/main/params/Params.scala
+++ 2019Qualify/src/main/params/Params.scala
@@ -8,8 +8,8 @@ object Params {
   def getSolver(implicit file: String) = file match {
     case "b_lovely_landscapes.txt" => SolverType.GreedyPhoto
     case "e_shiny_selfies.txt" => SolverType.GreedyPhoto
-    case "c_memorable_moments.txt" => SolverType.GreedyPhoto
-    case "a_example.txt" => SolverType.GreedyPhoto
+    case "c_memorable_moments.txt" => SolverType.HamiltonianPath
+    case "a_example.txt" => SolverType.GreedySlide
     case _ => SolverType.GreedySlide
   }
 
