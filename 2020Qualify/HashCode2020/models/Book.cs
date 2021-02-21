47759804b638ce4f2cbcee7dc2e0c2315045e163
Bruno Muratore <bruno.muratore@outlook.com>
2021-02-21 21:21:21 +0000

    models

---
+++ 2020Qualify/HashCode2020/models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCode2020.models
+{
+    public class Book
+    {
+        public int id;
+        public int score;
+    }
+}
---
+++ 2020Qualify/HashCode2020/models/Library.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCode2020.models
+{
+    public class Library
+    {
+        public int id;
+        public HashSet<Book> books;
+        public int capacity;
+        public int signup;
+    }
+}
